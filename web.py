20b703a13b1e29c7b99a104f46859b47d8c9e434
asheralerca <ndwi4231@gmail.com>
2025-05-18 13:19:29 +0700

    crud-kategori

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from flask import Flask
 from auth import auth_bp
 from dashboard import dashboard_bp
 from detail_mk import detailmk_bp
+from kategori import kategori_bp
 
 app = Flask(__name__)
 app.secret_key = 'sipadtei2025'
@@ -11,6 +12,7 @@ app.secret_key = 'sipadtei2025'
 app.register_blueprint(auth_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(detailmk_bp)
+app.register_blueprint(kategori_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
